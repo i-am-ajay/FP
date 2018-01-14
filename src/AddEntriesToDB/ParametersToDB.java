628916f062de841924662b69996c223e4aeac997
ajay chandel <ajaychandel.1987@gmail.com>
2018-01-15 01:15:39 +0530

    final

--- src/AddEntriesToDB/ParametersToDB.java
+++ src/AddEntriesToDB/ParametersToDB.java
@@ -63,7 +63,8 @@ public class ParametersToDB {
 			statement.registerOutParameter(2, Types.DECIMAL);
 			statement.registerOutParameter(3, Types.VARCHAR);
 			ResultSet parameterData = statement.executeQuery();
-			String weight = statement.getString(2);
+			Double val = statement.getDouble(2);
+			String weight = val.toString();
 			String remark = statement.getString(3);
 			paramArray = new String[]{weight,remark};
 		} catch (SQLException e) {
